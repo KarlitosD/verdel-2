00cbe89792fff1324314cd0f99094464f359bcd6
cajitadecarton <joaconizzo@gmail.com>
2022-11-05 19:41:18 -0300

    mostrar listas en el menu

--- components/menu.js
+++ components/menu.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import useSWR from "swr";
 
 
 export default function Menu() {
+    const {data : lists} = useSWR('/api/lists')
 
+    return (
+    <>
+      <div>
+        {lists?.map(list => (
+            <div key={list.id}>
+                <h1>{list.name}</h1>
+            </div>
+        ))}
+      </div>
+    </>
+  );
 }
