f151cc23df3f69144c488a8ffb630a4b07aec027
KarlitosD <carlosdcampos.04@gmail.com>
2022-11-05 18:32:33 -0300

    un archivo con los colores de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-const COLORS = [ 'slate', 'gray', 'zinc', 'neutral', 'stone', 'red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose' ]
-const COLOR_A = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "900"]
+const { COLORS, COLOR_A } = require("./utils/colors")
 
 const generateColorSafe = preffix => {
   const colorWithout = COLORS.map(color => COLOR_A.map(i => `${preffix}-${color}-${i}`)).flat(1)
---
+++ utils/colors.js
@@ -0,0 +1,4 @@
+const COLORS = [ 'slate', 'gray', 'zinc', 'neutral', 'stone', 'red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose' ]
+const COLOR_A = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "900"]
+
+module.exports = { COLORS, COLOR_A }
