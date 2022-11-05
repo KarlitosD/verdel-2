a8504bbcdc7ef43a4ad532bf05bc46964c77f78f
KarlitosD <carlosdcampos.04@gmail.com>
2022-11-05 04:10:45 -0300

    Crear lista y agregar usuario a la lista

--- pages/api/lists/[id].js
+++ pages/api/lists/[id].js
@@ -1,3 +1,9 @@
+import { unstable_getServerSession } from "next-auth/next"
+// import { uid } from "uid/secure"
+import { authOptions } from 'pages/api/auth/[...nextauth]'
+import Models from "/db/models/index.js"
+const { List, User } = Models
+
 const SECTIONS = [
     {
       name: "Verduleria",
@@ -25,6 +31,40 @@ const LISTS = [{
     sections: SECTIONS
 }]
 
+const handlers = {
+  async GET(req, res, session){
+      // const { id } = session.user
+      // const lists = await List.findAll()
+      res.send(LISTS)
+  },
+  async POST(req, res, session){
+      const { id: listId } = req.query
+      console.log(listId)
+      const [list, user] = await Promise.all([
+          List.findOne({ where: { id: listId } }), 
+          User.findOne({ where: { id: session.user.id },  attributes: ['id'] })
+      ])
+      // console.log({ user })
+      if(!list) throw new Error("List not found")
+      await list.addUser(user)
+      // const newList = await List.create({
+      //     id: uid(20),
+      //     name,
+      //     creatorId: 
+      // })
+      res.send({ message: "User added in the list" })
+  }
+}
+
 export default async function handler(req, res){
-    res.json(LISTS[0])
+  try {
+      console.log("Uwu")
+      const session = await unstable_getServerSession(req, res, authOptions)
+      if(!session) throw new Error("No autorizado")
+      const fn = handlers[req.method]
+      if(fn) await fn(req, res, session)
+  } catch (error) {
+      console.log(error.message)
+      res.status(400).send({ message: error.message })
+  }
 }
--- pages/api/lists/index.js
+++ pages/api/lists/index.js
@@ -1,33 +1,37 @@
 import { unstable_getServerSession } from "next-auth/next"
-import { uid } from "uid"
+import { uid } from "uid/secure"
 import { authOptions } from 'pages/api/auth/[...nextauth]'
-import { Group } from "/db/models"
+import Models from "/db/models/index.js"
+const { List } = Models
 
 const handlers = {
     async GET(req, res, session){
         // const { id } = session.user
-        const groups = await Group.findAll()
-        res.send(groups)
+        const lists = await List.findAll()
+        res.send(lists)
     },
-    async POST(req, res){
-        
+    async POST(req, res, session){
         const { name } = req.body
-        
-        const newGroup = await Group.create({
+        const newList = await List.create({
             id: uid(20),
-            name
+            name,
+            creatorId: session.user.id
         })
-        res.send({ message: "Group created" })
+        console.log(newList)
+        // const newList = {}
+        res.send({ message: "List created", list: newList })
     }
 }
 
 export default async function handler(req, res){
     try {
-        const session = unstable_getServerSession(req, res, authOptions)
+        console.log("Uwu")
+        const session = await unstable_getServerSession(req, res, authOptions)
         if(!session) throw new Error("No autorizado")
         const fn = handlers[req.method]
         if(fn) await fn(req, res, session)
     } catch (error) {
+        console.log(error.message)
         res.status(400).send({ message: error.message })
     }
 }
