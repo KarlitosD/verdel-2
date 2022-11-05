4db07848ed00e642f3476530435cb348d0445387
cajitadecarton <joaconizzo@gmail.com>
2022-11-05 18:49:44 -0300

    creado componente menu

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Menu from './components/menu'
+import Menu from '../components/menu'
 // import styled from "styled-components"
 import { unstable_getServerSession } from "next-auth/next"
 import { authOptions } from './api/auth/[...nextauth]'
