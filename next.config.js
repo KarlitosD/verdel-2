d2e5b4d1fa384483c2cbc198a1dff1b4a8c784ea
KarlitosD <carlosdcampos.04@gmail.com>
2022-11-06 00:17:50 -0300

    eliminar styled components de next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
+  swcMinify: true
 }
 
 module.exports = nextConfig
