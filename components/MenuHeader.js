470c7bc345c91b99698f9c31fb18e1ba078d6e98
KarlitosD <carlosdcampos.04@gmail.com>
2022-11-07 03:41:49 -0300

    arreglar error al intentar cargar muy rapido la foto de perfil

--- components/MenuHeader.js
+++ components/MenuHeader.js
@@ -2,18 +2,21 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { useTheme } from "next-themes";
 
+
 export default function MenuHeader() {
   const { theme, setTheme } = useTheme();
   const { data } = useSession();
   return (
     <div className="bg-orange-500 flex w-full justify-between py-2 px-5">
-      <Image
-        src={data?.user?.image}
-        width={50}
-        height={50}
-        alt=""
-        className="rounded-full"
-      />
+      {
+        data?.user?.image && <Image
+          src={data?.user?.image}
+          width={50}
+          height={50}
+          alt="Imagen del perfil"
+          className="rounded-full"
+        />
+      }
       <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>toggle</button>
       <button className="text-4xl text-black dark:text-white">+</button>
     </div>
