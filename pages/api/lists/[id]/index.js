4918cd78e2111d3a6af45fc01de5665e0cc77e1a
KarlitosD <carlosdcampos.04@gmail.com>
2022-11-07 03:36:07 -0300

    pagina de invitacion

--- pages/api/lists/[id]/index.js
+++ pages/api/lists/[id]/index.js
@@ -4,12 +4,18 @@ const { List, User, Section, Product } = Models
 
 const handlers = {
   async GET(req, res){
-    const { id: listId } = req.query
-    const list = await List.findByPk(listId, {
-      include: [
+    const { id: listId, include="all" } = req.query
+    const includes = {
+      all: [
         { model: User, through: { attributes: [] } },
         { model: Section, include: Product },
-      ]
+      ],
+      users: { model: User, through: { attributes: [] } },
+      sections: { model: Section},
+      products: { model: Section, include: Product },
+    }
+    const list = await List.findByPk(listId, {
+      include: includes[include]
     })
     if(!list) throw { status: 404, message: "List not found" }
     res.send(list)
