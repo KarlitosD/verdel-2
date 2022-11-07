52e09b2ee9db7d6a8f29c223912623f093fa6251
cajitadecarton <joaconizzo@gmail.com>
2022-11-07 15:31:03 -0300

    miembros de la lista mostrados en el header

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,22 @@
 import Jazzicon from "react-jazzicon";
 
-export default function Header({ listTitle }) {
-    return (
+export default function Header({ name, users }) {
+  console.log(users);
+  return (
     <div className="bg-orange-500 flex items-center w-full py-2 px-5">
-        <Jazzicon
-                  diameter={40}
-                  seed={Number(
-                    listTitle
-                      ?.split("")
-                      .map((l) => l.charCodeAt())
-                      .join("")
-                  )}
-                />
-        <h1 className="ml-4 text-gray-800 font-extrabold text-lg">
-            {listTitle}
-        </h1>
+      <Jazzicon
+        diameter={40}
+        seed={Number(
+          name
+            ?.split("")
+            .map((l) => l.charCodeAt())
+            .join("")
+        )}
+      />
+      <div className="ml-4">
+        <h1 className="text-gray-800 font-extrabold text-lg">{name}</h1>
+        <h2 className="text-black font-semibold">{users.map((user) => user.name).join(", ")}</h2>
+      </div>
     </div>
   );
 }
