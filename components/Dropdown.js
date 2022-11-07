60957ad97d2f8fdf01e4073160bfd5b1caaccf8d
cajitadecarton <joaconizzo@gmail.com>
2022-11-07 16:05:35 -0300

    dropdown sin terminar

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,46 +1,31 @@
-import Link from "next/link";
-import { useState } from "react";
+import { setTheme, useTheme } from "next-themes"
 
-export default function Dropdown({ children, options }) {
-  const [showOptions, setShowOptions] = useState(false);
-  const handleClick = () => {
-    setShowOptions(!showOptions);
-  };
+export default function Dropdown({ children }) {
+  const { theme, setTheme } = useTheme();
   return (
     <div className="relative inline-block text-left">
       <div>
-        <button
-          onClick={handleClick}
-          id="menu-button"
-          aria-expanded="true"
-          aria-haspopup="true"
-        >
+        <button id="menu-button" aria-expanded="true" aria-haspopup="true">
           {children}
         </button>
       </div>
-      {showOptions && (
-        <div
-          className="absolute right-0 z-10 mt-2 w-56 origin-top rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="menu-button"
-          tabIndex="-1"
-        >
-          {options &&
-            options.map((option) => (
-              <Link
-                key={option}
-                href="/"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                role="menuitem"
-                tabIndex="-1"
-                id="menu-item-0"
-              >
-                {option}
-              </Link>
-            ))}
+      <div
+        className="absolute left-0 z-10 mt-2 p-2 w-max origin-top-left rounded-md bg-gray-200 dark:bg-gray-700"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="menu-button"
+        tabIndex="-1"
+      >
+        <div className="py-1" role="none">
+          <button
+            className="bg-black rounded-2xl dark:bg-white"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            toggle
+          </button>
+          <button className="ml-4 text-red-500 font-semibold">Logout</button>
         </div>
-      )}
+      </div>
     </div>
   );
 }
--- components/MenuHeader.js
+++ components/MenuHeader.js
@@ -1,26 +1,24 @@
 import Image from "next/image";
 import { useSession } from "next-auth/react";
-import { useTheme } from "next-themes";
 import Dropdown from "./Dropdown";
 
 export default function MenuHeader() {
-  const { theme, setTheme } = useTheme();
   const { data } = useSession();
-  const options = ["Modo Oscuro", "Cerrar Sesión"]
+  const options = ["Modo Oscuro", "Cerrar Sesión"];
   return (
     <div className="bg-orange-500 flex w-full justify-between py-2 px-5">
       <Dropdown options={options}>
-      {data?.user?.image && (
-        <Image
-          src={data?.user?.image}
-          width={40}
-          height={40}
-          alt="Imagen del perfil"
-          className="rounded-full"
-        />
-      )}
+        {data?.user?.image && (
+          <Image
+            src={data?.user?.image}
+            width={40}
+            height={40}
+            alt="Imagen del perfil"
+            className="rounded-full"
+          />
+        )}
       </Dropdown>
-      <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>toggle</button>
+
       <button className="text-4xl text-black">+</button>
     </div>
   );
