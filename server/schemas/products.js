dd8264c8890bd34d94fbd393f5f5b62a076154cb
KarlitosD <carlosdcampos.04@gmail.com>
2022-11-07 16:02:54 -0300

    poner opcionales campos que al final no usamos

--- server/schemas/products.js
+++ server/schemas/products.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createProductSchema = z.object({
     name: z.string().max(300),
-    description: z.string().max(200)
+    description: z.string().max(200).optional()
 }) 
 
 export const editProductSchema = z.object({
