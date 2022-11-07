065708988e753693419ae1610bc44de4c802dccf
ITERUM <nicolasgomez.110503@gmail.com>
2022-11-07 15:53:05 -0300

    header

--- components/Header.js
+++ components/Header.js
@@ -4,20 +4,6 @@ export default function Header({ name, users }) {
   console.log(users);
   return (
     <div className="bg-orange-500 flex items-center w-full py-2 px-5">
-<<<<<<< HEAD
-        <Jazzicon
-                  diameter={45}
-                  seed={Number(
-                    listTitle
-                      ?.split("")
-                      .map((l) => l.charCodeAt())
-                      .join("")
-                  )}
-                />
-        <h1 className="ml-4 text-gray-800 font-extrabold text-lg">
-            {listTitle}
-        </h1>
-=======
       <Jazzicon
         diameter={40}
         seed={Number(
@@ -31,7 +17,6 @@ export default function Header({ name, users }) {
         <h1 className="text-gray-800 font-extrabold text-lg">{name}</h1>
         <h2 className="text-black font-semibold">{users.map((user) => user.name).join(", ")}</h2>
       </div>
->>>>>>> ce0a4cfa52f70d8291513543492443de1361562d
     </div>
   );
 }
