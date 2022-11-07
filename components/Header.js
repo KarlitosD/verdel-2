7c177e8709f114049b1834107d0f29eaeb08b254
cajitadecarton <joaconizzo@gmail.com>
2022-11-06 21:45:20 -0300

    componente header colocado en index

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@
 
 export default function Header() {
     return (
-    <div className="bg-orange-300 flex w-full justify-between py-1 px-5">
+    <div className="bg-orange-300 flex w-full justify-between py-1 px-5 h-[58px]">
         <h1>
             Nombre de la lista
         </h1>
